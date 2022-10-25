81b18d8dd84f64bddcb784e13eff4da42416ee8c
etoga <etoga@dtu.dk>
2022-10-25 13:11:36 +0200

    Added various testing facilities

--- tests/variation_runner.sh
+++ tests/variation_runner.sh
@@ -2,7 +2,6 @@
 
 # Variations to run
 declare -a variations=(
-  "-DMULTISCALE=0"
   ""
   "-DTHICC_SEP=1"
   "-DRECALC=2"
@@ -11,12 +10,18 @@ declare -a variations=(
 
 variant=0
 
+mkdir out_raw
+mkdir skeletons
+
 for i in "${variations[@]}"
 do
   # Build with compile time option
   cmake -G Ninja -DCMAKE_MAKE_PROGRAM=ninja -DCMAKE_CXX_FLAGS="$i -O2 -DCORE_TEST=16 -DCORE_TEST_SEC=2" -S .. -B ../cmake-build-default -DCMAKE_BUILD_TYPE=Release
   cmake --build ../cmake-build-default
 
+  # Make directories
+  mkdir "skeletons/var$((++variant))"
+
   # Run
-  ls -1 /work3/etoga/3DMeshes/[^0-9]* | ./runtime_test.sh 3 "var$((++variant))"
+  ls -1 /work3/etoga/3DMeshes/[^0-9]* | ./runtime_test.sh 3 "var$variant"
 done
