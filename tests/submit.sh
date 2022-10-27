b0a0d64634657347a5cf886b86a4213d4fe3e3fd
etoga <etoga@dtu.dk>
2022-10-27 10:52:49 +0200

    Small update to test script

--- tests/submit.sh
+++ tests/submit.sh
@@ -5,13 +5,13 @@
 ### -- set the job Name --
 #BSUB -J Skeletonization
 ### -- ask for number of cores (default: 1) --
-#BSUB -n 16
+#BSUB -n 32
 ### -- specify that the cores must be on the same host --
 #BSUB -R "span[hosts=1]"
 ### -- specify that we need 2GB of memory per core/slot --
 #BSUB -R "rusage[mem=1GB]"
 ### -- Request specific CPU
-##BSUB -R "select[model == XeonGold6226R]"
+#BSUB -R "select[model == XeonGold6226R]"
 ### -- specify that we want the job to get killed if it exceeds 3 GB per core/slot --
 #BSUB -M 3GB
 ### -- set walltime limit: hh:mm --
